14a5d6beeb0033daae208018e6a59643e8f1caac
annasitsiliya <annasitsiliya@gmail.com>
2022-09-14 23:55:22 +0200

    clean up

--- src/test/java/org/asurus/PetsApiTest.java
+++ src/test/java/org/asurus/PetsApiTest.java
@@ -16,27 +16,32 @@ import java.util.List;
 public class PetsApiTest {
     private PetService petService;
     private Pet pet;
+
     @BeforeClass
     public void init() {
         petService = new PetService();
         pet = createPet();
     }
+
     @BeforeMethod
     public void setup() {
         petService.sendPostRequest("pet", pet);
     }
+
     @Test
     public void getPet() {
         Response response = petService.sendGetRequest("pet/55");
         Pet actualPet = response.getBody().as(Pet.class);
         Assert.assertEquals(actualPet, pet);
     }
+
     @Test
     public void postPet() {
         Response response = petService.sendPostRequest("pet", pet);
         Pet actualPet = response.getBody().as(Pet.class);
         Assert.assertEquals(actualPet, pet);
     }
+
     @Test
     public void deletePet() {
         Response response = petService.sendDeleteRequest("pet/55");
@@ -44,6 +49,7 @@ public class PetsApiTest {
         Assert.assertTrue(actualBody.contains("55"), "Body doesn't contain pet id");
         Assert.assertTrue(actualBody.contains("200"), "Body doesn't contain OK status code");
     }
+
     private Pet createPet() {
         Category category = new Category().setId(43).setName("Taksa");
         Tag tag1 = new Tag().setId(12).setName("brown");
